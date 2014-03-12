2d9861d4160c69e5c94136a94e904cf3bf322301
Alex Hertz <hey@hey.al>
2014-03-12 02:01:02 -0500

    Well that just turned into an insane amount of work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+
+// Sticky nav snapping
+$('#navbar').waypoint('sticky', {
+  wrapper: '<div class="sticky-wrapper" />',
+  stuckClass: 'stuck'
+});
+
+// Smooth scroll links if anchor on same page
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 2000);
+        return false;
+      }
+    }
+  });
+});
