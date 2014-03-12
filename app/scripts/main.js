86ea3cce8c346f95ce5116723363d54db5dfdfe2
Alex Hertz <hey@hey.al>
2014-03-12 16:32:08 -0500

    Video Backgrounds Are Us
    Tested in latest versions of chrome, safari, firefox. Firefox seems to
    be having a hellofuh time with it.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,20 @@
+$(function(){
 
-// Sticky nav snapping
-$('#navbar').waypoint('sticky', {
-  wrapper: '<div class="sticky-wrapper" />',
-  stuckClass: 'stuck'
-});
+  // Sticky nav snapping
+  $('#navbar').waypoint('sticky', {
+    wrapper: '<div class="sticky-wrapper" />',
+    stuckClass: 'stuck'
+  });
 
-// Smooth scroll links if anchor on same page
-$(function() {
+  // Clip video
+  $(function(){
+    $('.video--wrapper').css({'height':(($(window).height())), 'width':(($(window).width())), 'overflow':'hidden'});
+    $(window).resize(function(){
+      $('.video--wrapper').css({'height':(($(window).height())), 'width':(($(window).width())), 'overflow':'hidden'});
+    });
+  });
+
+  // Smooth scroll links if anchor on same page
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
